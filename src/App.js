9efbf4076c3f578eddcbb526e5e3e888445e517c
MoizFayyaz123 <moizfayyaz1@gmail.com>
2023-08-05 00:52:37 +0500

    Components folder and screen folder added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './screens/Home';
 
 function App() {
   return(
-  <div ><Navbar></Navbar></div>
+  <div><Home></Home></div>
   );
 }
 
