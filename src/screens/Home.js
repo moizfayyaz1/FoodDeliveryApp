0841672ca727fa575f27c3d22dadf81b51ac76d3
MoizFayyaz123 <moizfayyaz1@gmail.com>
2023-08-05 21:38:27 +0500

    react router dom and components

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 
 export default function Home() {
   return (
     <div>
         <div><Navbar/></div>
-        <div>Home</div>
+        <div><Carousel></Carousel></div>
+        <div><Card></Card></div>
         <div><Footer></Footer></div>
+        
     </div>
   )
 }
